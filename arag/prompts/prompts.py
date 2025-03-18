d00f429b0342ef7c3f43e090a90d0ecc9252264c
aboros98 <borosandrei1998@gmail.com>
2025-03-18 20:25:40 +0200

    Update prompts. Add processer status

--- arag/prompts/prompts.py
+++ arag/prompts/prompts.py
@@ -3,11 +3,11 @@ from .concise_answer import CONCISE_ANSWER
 from .decision import DECISION
 from .default import DEFAULT
 from .evaluator import EVALUATOR
-from .formatter import FORMATTER
 from .improver import IMPROVER
 from .knowledge import KNOWLEDGE
 from .knowledge_gaps import KNOWLEDGE_GAPS
 from .missing_info import MISSING_INFO
+from .process import PROCESS
 from .query_rewrite import QUERY_REWRITE
 
 PROMPTS = {
@@ -20,7 +20,6 @@ PROMPTS = {
     "improver": IMPROVER,
     "knowledge": KNOWLEDGE,
     "missing_info": MISSING_INFO,
-    "formatter": FORMATTER,
-    "concise_answer": CONCISE_ANSWER
-
+    "concise_answer": CONCISE_ANSWER,
+    "process": PROCESS
 }
