cacfa587f9e7acfb8cc1de00ef6da494d5bb5eee
aboros98 <borosandrei1998@gmail.com>
2025-03-27 16:33:33 +0200

    bugfix

--- arag/arag_agents/query_rewrite_agent.py
+++ arag/arag_agents/query_rewrite_agent.py
@@ -1,8 +1,8 @@
 from typing import List
 
-from decorators.agent_registry import register_action
 from pydantic import BaseModel
 
+from .decorators.agent_registry import register_action
 from .template_agent import BaseAgent
 from .utils.agent_primitives import client_sturctured_message
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="arag",
-    version="0.4.3",
+    version="0.5.0",
     packages=find_packages(),
     install_requires=[
         "openai",
