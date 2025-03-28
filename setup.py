4ef4601d95cfd904e66de2d7f6bd2143f2f39b6a
aboros98 <borosandrei1998@gmail.com>
2025-03-28 14:09:19 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="arag",
-    version="0.6.7",
+    version="0.6.8",
     packages=find_packages(),
     install_requires=[
         "openai",
